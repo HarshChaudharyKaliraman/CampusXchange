f09372740dbd4df21dedcd667b366087668b076b
Harsh Chaudhary <149260243+HarshChaudharyKaliraman@users.noreply.github.com>
2026-04-04 16:52:51 +0530

    Add files via upload

---
+++ server/src/controllers/authController.js
@@ -0,0 +1,24 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
+export const register = async (req, res) => {
+    try{
+        const { name, email, password } = req.body;
+
+        const existingUser = await User.findOne({ email });
+        if(existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const user = await User.create({
+            name, 
+            email,
+            passwordHash,
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
