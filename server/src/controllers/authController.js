21a5ebadcf586a3cf838051051a165933046958b
Harsh Chaudhary <149260243+HarshChaudharyKaliraman@users.noreply.github.com>
2026-04-07 15:21:31 +0530

    Update authController.js

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
     try{
@@ -17,6 +18,35 @@ export const register = async (req, res) => {
             name, 
             email,
             passwordHash,
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const login = async (req, res) => {
+    try{
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.passwordHash);
+        if(!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        const token = jwt.sign(
+            { id: user._id, role: user.role },
+            process.env.JWT_SECRET,
+            { expiresIn: "7d" }
+        );
+
+        res.json({
+            message: "Login successful",
+            token,
         });
     } catch (error) {
         res.status(500).json({ message: error.message });
